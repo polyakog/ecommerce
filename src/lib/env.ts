64febba5e0bc8f86cf1f61cb4735b17864c838d4
Gennadij <gpolyakov77@gmail.com>
2023-10-18 15:31:14 +0400

    feat: add oAuth

---
+++ src/lib/env.ts
@@ -0,0 +1,11 @@
+import {z} from "zod"
+
+const envSchema = z.object({
+DATABASE_URL: z.string().min(1),
+GOOGLE_CLIENT_ID: z.string().min(1),
+GOOGLE_CLIENT_SECRET: z.string().min(1),
+NEXTAUTH_URL: z.string().min(1),
+NEXTAUTH_SECRET: z.string().min(1),
+})
+
+export const env = envSchema.parse(process.env)
