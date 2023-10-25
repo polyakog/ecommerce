e388ca7a75d5a8aa6be994ff3aaa683a07bd5a3d
Gennadij <gpolyakov77@gmail.com>
2023-10-25 20:02:16 +0400

    feat: add admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import exp from "constants"
+
+const Admin = () => {
+    return (
+        <div>
+            <h1 className="mb-3 font-bold text-lg">Страница администратора</h1>
+
+        </div>
+    )
+}
+
+export default Admin
