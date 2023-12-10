a50da60c4570d0e2174c213ecd804dcc0f8a1634
Gennadij <gpolyakov77@gmail.com>
2023-12-10 21:37:22 +0400

    feat: change register page

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,17 +1,26 @@
-const POST =async (request: Request) => {
+import { NextResponse } from "next/server"
+import {hash} from 'bcrypt'
+
+
+export const POST =async (request: Request) => {
 
     try {
         const {email, password} = await request.json()
         // валидация email и password
         console.log({email, password})
+
+        const hashedPassword = await hash(password, 10)
         
     } catch (error) {
         console.log({error})
         
     }
+
+    return NextResponse.json({message: 'success'})
+
+    
     
 }
 
 
 
-export default POST
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,55 +1,12 @@
 import FormButton from "@/components/FormButton"
+import Form from "./form";
 
-async function handleSubmit(formData:FormData) {
-    "use server"
-
-    const email = formData.get("email"?.toString())
-    const password = formData.get("password"?.toString())
-    const repassword = formData.get("repassword"?.toString())
-    
-    if (!email || !password || !repassword) {
-        throw Error("Не заполнены обзятельные поля")
-    } else if (password !== repassword) {
-        throw Error("Не совпадает повторный пароль")
-    }
-
-
-    console.log(email, password)
-    
-}
+ 
 
 const Register = () => {
- 
+
     return (
-        <form className="flex flex-col gap-2 mx-auto items-center" action={handleSubmit} >
-
-            <input
-                required
-                name="email"
-                className="input input-bordered w-full mb-3 max-w-xs"
-                placeholder="введите email"
-                type="email"
-            />
-
-            <input
-                required
-                name="password"
-                className="input input-bordered w-full mb-3 max-w-xs"
-                placeholder="введите пароль"
-                type="password"
-            />
-
-            <input
-                required
-                name="repassword"
-                className="input input-bordered w-full mb-3 max-w-xs"
-                placeholder="введите повторно пароль"
-                type="password"
-            />
-
-            <FormButton className="w-full max-w-xs">Зарегистрировать</FormButton>
-
-        </form>
+        <Form/>
     )
 }
 
