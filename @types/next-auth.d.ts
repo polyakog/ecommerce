db401ef688e4941f5cbc1656d374fd5dcc273cd4
Gennadij <gpolyakov77@gmail.com>
2024-02-02 09:45:51 +0400

    feat: add role for user in db and session type

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -3,7 +3,8 @@ import {DefaultSession} from "next-auth"
 declare module "next-auth" {
     interface Session{
         user: {
-            id: string
+            id: string,
+            role: string,
         } & DefaultSession["user"]
     }
 }
