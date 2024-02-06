79f6366231594b94f8d372814dc524b534f33eaf
Gennadij <gpolyakov77@gmail.com>
2024-02-06 18:48:39 +0400

    feat: add redirect in register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,14 +2,15 @@ import FormButton from "@/components/FormButton"
 import RegisterForm from "@/components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
  
 
 const Register = async () => {
-    // const session = await getServerSession()
-    // if (session) {
-    //     redirect("/")
-    // }
+    const session = await getServerSession(authOptions)
+    if (session) {
+        redirect("/")
+    }
 
     return (
         <RegisterForm/>
